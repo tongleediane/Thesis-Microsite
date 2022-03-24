998514484e05df75c9221cacb6cad4d2fec25696
tongli <tongleediane@gmail.com>
2022-03-24 10:02:05 -0700

    Update s1.js

--- s1.js
+++ s1.js
@@ -144,8 +144,10 @@ sketch.setup = function() {
   
   sSlider = sketch.createSlider(10,150,100);
   sSlider.position(70, 250);
-  sSlider.style('width', '200px');};
+  sSlider.style('width', '200px');
   sSlider.addClass("mySliders");
+};
+
   // sSlider.sketch.addClass("Slider");
 
 
